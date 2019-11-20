e70ebe99685e2dbcb15108f91b6312a9cce9d984
Josh Ponelat <jponelat@gmail.com>
2019-11-20 15:55:45 +0200

    add zoom wars

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import ZoomWars from './zoom-wars-plain.svg';
+import Yaml from 'yaml'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Build something fun!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    players: []
+  }
+
+  constructor(props) {
+    super(props)
+    this.fetchPlayers()
+  }
+
+  fetchPlayers = () => {
+    fetch('players.yaml')
+      .then( r => r.text())
+      .then(Yaml.parse)
+      .then(players => {
+        players.sort((a,b) => b.count - a.count)
+        this.setState({ players, err: '', playerLoading: false })
+      })
+      .catch(err => {
+        this.setState({ err: err+'', playerLoading: false })
+      })
+  }
+
+  render() {
+    const { players } = this.state
+    let winningScore
+    if(players.length)
+      winningScore = players[0].count
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ZoomWars} className="App-logo" alt="logo" />
+
+          <hr className="divider"/>
+
+          <table className="players">
+            { players.map(({name, count}, i) => (
+              <tr className={ count === winningScore ? "winning" : ""}
+                key={name}>
+                <td>{name}</td>
+                <td>{count}</td>
+              </tr>
+            ))}
+          </table>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
