b65f3dc115530b70eb73c52965ebfc080a186f17
Josh Ponelat <jponelat@gmail.com>
2019-11-20 16:17:33 +0200

    Updates

---
+++ precache-manifest.580d603dbcaf140bb193a9e4c88368c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322f6c30e973704eca32e50cf4edc30f",
+    "url": "/zoom-wars/index.html"
+  },
+  {
+    "revision": "e8dd9c1f4886483b4ad1",
+    "url": "/zoom-wars/static/css/main.5ccbd506.chunk.css"
+  },
+  {
+    "revision": "b5728cb5124b45111cbb",
+    "url": "/zoom-wars/static/js/2.23cf039a.chunk.js"
+  },
+  {
+    "revision": "e8dd9c1f4886483b4ad1",
+    "url": "/zoom-wars/static/js/main.132ecca1.chunk.js"
+  },
+  {
+    "revision": "5b3e7856bd600fc386ab",
+    "url": "/zoom-wars/static/js/runtime~main.fcb39e59.js"
+  },
+  {
+    "revision": "5eb22ea541666adfe2dd264769a462d0",
+    "url": "/zoom-wars/static/media/zoom-wars-plain.5eb22ea5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.930964c192f511e2707b288d65f819a7.js"
+  "/zoom-wars/precache-manifest.580d603dbcaf140bb193a9e4c88368c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zoom-wars/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
