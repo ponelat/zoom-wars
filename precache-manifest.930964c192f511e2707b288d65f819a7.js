5124cef39d6dba42a2a6aa021416cc57a64e290f
Josh Ponelat <jponelat@gmail.com>
2019-11-20 16:07:35 +0200

    Updates

---
+++ precache-manifest.930964c192f511e2707b288d65f819a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19473103ad7773aa4be7537fcf00936c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a6c28e30c9c85e9352",
+    "url": "/static/css/main.5ccbd506.chunk.css"
+  },
+  {
+    "revision": "b5728cb5124b45111cbb",
+    "url": "/static/js/2.23cf039a.chunk.js"
+  },
+  {
+    "revision": "79a6c28e30c9c85e9352",
+    "url": "/static/js/main.f8311712.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5eb22ea541666adfe2dd264769a462d0",
+    "url": "/static/media/zoom-wars-plain.5eb22ea5.svg"
+  }
+]);
